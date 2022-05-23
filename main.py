4ddc5a4614d80eb56df248affcbd832795b34031
FABIO PAULI <102900009+FabioPauli1@users.noreply.github.com>
2022-05-23 15:38:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+num1 = float(input('Numero 1: '))
+ope = str(input('Digite a operação +, -, * ou /: '))
+num2 = float(input('Numero 2: '))
+
+if ope == '+':
+  print(num1 + num2)
+elif ope == '-':
+  print(num1 - num2)
+elif ope == '*':
+  print(num1 * num2)
+elif ope == '/':
+  print(num1 / num2)
